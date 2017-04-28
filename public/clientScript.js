988d7b6a9651e94e04614dc09dc42e3474971fca
greenitthe <greenitthe@gmail.com>
2017-04-28 13:59:15 -0500

    Loaf n' Fonts
    Catching up on back commits from an older computer

--- public/clientScript.js
+++ public/clientScript.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
     socket.emit('userReqUpdate', {
       playerID: "Wilson Grumpstache"
     });
-    
+
     gameLoop();
     requestAnimationFrame(update)
   }
@@ -101,39 +101,93 @@ $(document).ready(function() {
   //DOM elements
   var canvas = $('#gameCanvas')[0];
   var ctx = canvas.getContext("2d");
-  
-  
+
+
   //Example of animating a sprite
   var state = 0;
   var delay = 0;
-  
+  var mX;
+  var mY;
+  var buttons = [];
+  var texts = [];
+  $("#gameCanvas").bind('mousemove', function(e) {
+    mX = Math.round(e.pageX - $(this).offset().left);
+    mY = Math.round(e.pageY - $(this).offset().top);
+  });
+  $("#gameCanvas").click(function() {
+    console.log("clicked at: " + mX + " " + mY)
+    buttons.map(function(item) {
+      if (mX > item.x && mX < item.x+item.width && mY > item.y && mY < item.y+item.height) {
+        item.clicked();
+      }
+    })
+    console.log(buttons)
+  })
+
+  function createImage(path) {
+    var newImage = new Image();
+    newImage.src = path;
+    return newImage;
+  }
+
+  function Button(image, x, y, width, height, onClick) {
+    this.image = image,
+    this.x = x,
+    this.y = y,
+    this.width = width,
+    this.height = height,
+    this.clicked = onClick,
+    this.draw = function () {
+      ctx.drawImage(this.image,this.x,this.y);
+    }
+  }
+
+  function Text(phrase, x, y) {
+    this.phrase = phrase,
+    this.x = x,
+    this.y = y,
+    this.draw = function() {
+      ctx.font="30px 8-BIT"
+      ctx.fillStyle="#C07B25"
+      ctx.fillText(this.phrase, this.x, this.y)
+    }
+  }
+
+  buttons.push(new Button(createImage("images/loaf.png"), canvas.width/40, canvas.height/40,128,128,function() {
+    console.log("Clicked loaf");
+  }));
+
+  texts.push(new Text("0 Bread", canvas.width/5, canvas.height/6))
+
   function gameLoop() {
     ctx.clearRect(0,0,canvas.width,canvas.height);
+    ctx.fillStyle="#da9538";
+    ctx.fillRect(0,0,canvas.width, canvas.height);
     delay >= 10 ? (delay = 0,
                     animateFrame()): ++delay;
-    
-    drawPlayerTile(0,0,state);
+
+    //drawPlayerTile(0,0,state);
+    buttons.map(function(item) {item.draw();});
+    texts.map(function(item) {item.draw();});
   }
-  
+
   function animateFrame() {
-    console.log("animating")
     state = state >= 7 ? 0 : ++state;
   }
-  
+
   function drawPlayerTile(x,y,tileNum) {
     //Currently involves a wrapper, for any tile past 10, it assumes a new line down in the y axis
     var tileWidth = 64;
     var tileHeight = tileWidth;
-    
+
     var playerSprites = new Image();
     playerSprites.src = "images/playerCharacter.png"
-    
-    console.log(tileNum)
+
     drawTile(playerSprites, (tileNum % 10) * tileWidth, Math.floor(tileNum/10) * tileHeight, x, y, tileWidth, tileHeight);
   }
   function drawTile(spriteSrc,tileX,tileY,x,y,tileWidth,tileHeight) {
     ctx.drawImage(spriteSrc, tileX, tileY, tileWidth, tileHeight, x, y, tileWidth, tileHeight);
   }
-  
+
   update();
 });
